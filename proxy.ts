a4d426917f6b7a08e63ca68e7f1424bf510ec314
naya <nastyasytnik6@gmail.com>
2026-06-27 08:36:39 +0300

    add proxy

---
+++ proxy.ts
@@ -0,0 +1,36 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+const privateRoutes = ['/profile', '/notes'];
+const publicRoutes = ['/sign-in', '/sign-up'];
+
+export function proxy(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  const accessToken = request.cookies.get('accessToken')?.value;
+
+  const isPrivateRoute = privateRoutes.some((route) =>
+    pathname.startsWith(route)
+  );
+
+  const isPublicRoute = publicRoutes.some((route) =>
+    pathname.startsWith(route)
+  );
+
+
+  if (!accessToken && isPrivateRoute) {
+    return NextResponse.redirect(new URL('/sign-in', request.url));
+  }
+
+
+  if (accessToken && isPublicRoute) {
+    return NextResponse.redirect(new URL('/profile', request.url));
+  }
+
+  return NextResponse.next();
+}
+
+
+export const config = {
+  matcher: ['/profile/:path*', '/notes/:path*', '/sign-in', '/sign-up'],
+};
